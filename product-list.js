b1b932c4b49e9969835c42753fd4e29e51d81a7b
precodepath <precodepath@gmail.com>
2021-04-22 08:16:08 +0530

    Javascript foundation

---
+++ product-list.js
@@ -0,0 +1,31 @@
+
+// alert('we are in externa file');
+
+/* 
+This is way to add multi commnets
+*/
+
+// let firstName = "Test";
+
+let listCars = ["Maruti", "TATA", "Suzuki", "Mahi", "toyta"];
+
+console.log(listCars[2], listCars[4]);
+
+let myAdress = {
+    single: 'A',
+    phone: 9090,
+    city: 'mum',
+    officeAddres: {
+        path: 'abc chouck',
+        buildNo: 301,
+        area: 'Bangloar'
+    },
+    visitedPlace: ['Mumb', 'Ban', 'Us', 'cand'],
+    fFood: {
+        friute: ['apple', 'mangor'],
+        dish: ['panner', 'biryani'],
+        seaFood: ['fish']
+    }
+};
+
+console.log(myAdress)
